0f5a9fef2f262d0e84442a213cae58822cad1ead
TarvikPng <126049186+TarvikPng@users.noreply.github.com>
2024-02-05 21:02:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".vicetextu", {
+    strings: ["Počítačům", "Fotbalu", "Hře na kytaru", "rybaření", "ježdění na kole"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+})
